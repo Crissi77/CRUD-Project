7fee01bb249df76311aa7191e8aafb6688eadcf3
NeerajPC <NeerajPC@LAPTOP-L2JIADCM>
2024-06-24 09:09:13 +0530

    Add project files.

---
+++ CRUD Project/Data/TechContext.cs
@@ -0,0 +1,13 @@
+﻿using CRUD_Project.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_Project.Data
+{
+    public class TechContext:DbContext
+    {
+        public TechContext(DbContextOptions<TechContext> options) : base (options) { }
+
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
+                     
---
+++ CRUD Project/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_Project.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Name field is required")]
+        public String Name { get; set; }
+
+        [Required(ErrorMessage = "Profile field is required")]
+        public String Profile { get; set; }
+
+
+    }
+}
